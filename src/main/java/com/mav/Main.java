8884bb0ead8189e4627a1642824ac16140d4b686
inzheneher <inzheneher@ya.ru>
2016-07-08 12:30:27 +0600

    data extraction from test.json by field name

--- src/main/java/com/mav/Main.java
+++ src/main/java/com/mav/Main.java
@@ -1,7 +1,10 @@
 package com.mav;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mav.conf.Motorcycle;
 import com.mav.conf.Vehicle;
 
 import java.io.IOException;
@@ -18,11 +21,16 @@ public class Main {
             ObjectMapper mapper = new ObjectMapper();
             InputStream resourceAsStream = Main.class.getResourceAsStream("/test.json");
             JsonNode jsonNode = mapper.readTree(resourceAsStream);
+            Motorcycle testObj = new Motorcycle();
+            testObj.setMotorcycleCarriage(jsonNode.get(2).has("motorcycleCarriage"));
+            System.out.println(testObj.getMotorcycleCarriage());
+            System.out.println(testObj.getVehicleStopProbability());
             System.out.println(mapper.readValue(jsonNode.get(0).toString(), Vehicle.class));
             System.out.println(mapper.readValue(jsonNode.get(1).toString(), Vehicle.class));
             System.out.println(mapper.readValue(jsonNode.get(2).toString(), Vehicle.class));
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        catch (JsonParseException e) { e.printStackTrace(); }
+        catch (JsonMappingException e) { e.printStackTrace(); }
+        catch (IOException e) { e.printStackTrace(); }
     }
 }
